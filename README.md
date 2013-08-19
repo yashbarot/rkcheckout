02f2713a8e0e5f1f13ab5654934e467a7fde40a5
Sander Mangel <sander@sandermangel.nl>
2013-08-19 14:52:38 +0200

    [0.1.0] First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Redkiwi_Rkcheckout
+
+v 0.1.0
+Setup: Add a custom block with the identifier 'checkout-register-benefits' for text in the checkout
---
+++ app/code/community/Redkiwi/Rkcheckout/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @category    Redkiwi
+ * @package     Redkiwi_Rkcheckout
+ * @author      Sander Mangel <mangel@redkiwi.nl>
+ */
+ 
+class Redkiwi_Rkcheckout_Helper_Data extends Mage_Core_Helper_Data
+{
+}
